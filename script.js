338ea87a44c354fe407f35295f572a6496f3ba4e
evelyy-n <106785375+evelyy-n@users.noreply.github.com>
2024-09-01 19:49:55 -0300

    a

--- script.js
+++ script.js
@@ -6,75 +6,36 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Você é a favor das cotas raciais?"
+        enunciado: "Você é a favor da COTA RACIAL?",
         alternativas: [
-            {
-                texto: "sim.",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
-            },
-            {
-                texto: "Não.",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
-            }
+            "Sim, com certeza!",
+            "Não, um vexame!"
         ]
     },
     {
-        enunciado: "Você acredita que o sistema de cotas é justo?"
+        enunciado: "Você acredita que o sistema de cotas é justo?",
         alternativas: [
-            {
-                texto: "Sim, porque ele serve como inclusão e reparação."
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
-            },
-            {
-                texto: "Não! A cota racial demonstra inferioridade a certas etnias em inclusões sociais e profissionais desenvolvendo, ainda, desigualdades."
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
-            }
+            "Sim, pois serve muito bem como uma inclusão e compensação histórica.",
+            "Não! A Cota Racial demonstra inferioridade a certas etnias/raças em inclusões sociais e profissionais, aonde pessoas ganham sem mérito próprio.",
         ]
     },
     {
-        enunciado: "As cotas para as universidades ajudarão a diminuir o racismo?"
+        enunciado: "Para você, as Cotas para as universidades irão ajudar a diminuir o racismo e discriminação?",
         alternativas: [
-            {
-                texto: "Sim, pois com essas oportunidades poderão incluir outras pessoas a grandes sistemas educacionais a fim de não serem discriminados."
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
-            },
-            {
-                texto: "Não, mesmo tendo oportunidades para entrar a grandes instituições de ensino, não diminuiria a discriminação social e de certo modo, só a aumentaria."
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
-            }
+            "Sim, pois com essas oportunidades poderão incluir outras pessoas a grandes instituições de ensino a fim de terem uma boa educação e consequentemente diminuir a discriminação.",
+            "Não. Mesmo tendo oportunidades para se adentrarem a grandes instituições de ensino, isso não diminuiria a discriminação social, e também, de certo modo, poderia até aumentar a discriminação."
         ]
     },
     {
-        enunciado: "Você usaria o sistema de cotas raciais?",
+        enunciado: "Você utilizaria o sistema de Cotas Raciais?",
         alternativas: [
-            {
-                texto: "Sim, pois apresenta vários benefícios",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Não, porque acredito não ser justo."
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? "
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
+            "Sim, pois me beneficiaria bastante e justo.",
+            "Não, pois não me beneficiaria e/ou não acho muito justo."
         ]
     },
 ];
 
-
 let atual = 0;
-let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
@@ -82,32 +43,31 @@ function mostraPergunta() {
         mostraResultado();
         return;
     }
-    perguntaAtual = perguntas[atual];
+    const perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
-    mostraAlternativas();
+    mostraAlternativas(perguntaAtual.alternativas);
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas(alternativas) {
+    for (const alternativa of alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.textContent = alternativa;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
-function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
+function respostaSelecionada(alternativaSelecionada) {
+    historiaFinal += alternativaSelecionada + " ";
     atual++;
     mostraPergunta();
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em 2049... Isso acabará!";
     textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
+    caixaAlternativas.textContent = "É isso, obrigado!";
 }
 
 mostraPergunta();
